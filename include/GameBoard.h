6b719404a90776714227cec5cffad67a3d5bccff
Cole <33649427+colehanson17@users.noreply.github.com>
2021-10-09 14:37:09 -0600

    Started on GameBoard

--- include/GameBoard.h
+++ include/GameBoard.h
@@ -1,15 +1,15 @@
 
 class GameBoard
 {
-
 private:
-    char board[3][3];
     char player1;
     char player2;
 
 public:
+    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};;
     GameBoard();
     void printBoard();
+    bool checkForWin();
     bool gameOver();
     char *turn(int player, int position);
 };
--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -1,7 +1,35 @@
+#include <iostream>
 #include "GameBoard.h"
 
-class GameBoard {
-
-    char board[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+GameBoard::GameBoard(){
 
 };
+
+bool GameBoard::checkForWin() {
+    // Move through board vertically
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            return true;
+    }
+    // Move through board horizontally
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i])
+            return true;
+    }
+    // Check diagnals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        return true;
+    
+    return false;
+}
+
+void GameBoard::printBoard()
+{
+    std::cout << "-------------" << std::endl;
+    std::cout << "| " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << " |" << std::endl;
+    std::cout << "| " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << " |" << std::endl;
+    std::cout << "| " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << " |" << std::endl;
+    std::cout << "-------------" << std::endl;
+}
